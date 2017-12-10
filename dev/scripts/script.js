71a12975481224401941fb3a5e4a272181294315
Dana Manoli <dmanoli1@gmail.com>
2017-12-10 16:32:49 -0500

    correcting previous initialization

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,6 +1,6 @@
 $(function() {
 	
-	var $hamburger = $(".hamburger");
+	let $hamburger = $(".hamburger");
 		$hamburger.on("click", function(e) {
 			$('header nav').toggleClass('overlay');
 			$('nav .nav__items').toggleClass('show');
